1ee7c17a4535f9c36bb7c638bd33c62c4c75a8e8
ghosh128 <ghosh128@umn.edu>
2019-03-31 13:57:30 -0500

    value update and variance plot

--- HW3/P5.py
+++ HW3/P5.py
@@ -9,14 +9,15 @@ Created on Thu Mar 28 11:54:36 2019
 import matplotlib.pyplot as plt
 
 sigma_osq = 1
-list_sigma_xsq = list(range(-100,100))
+list_sigma_xsq = list(range(0,100))
 sigma_zsq = 0.75
 list_sigma_tsq = []
 #%%
 for sigma_xsq in list_sigma_xsq:
     sigma_tsq = 1
     for i in range(1,11):
-        sigma_tsq = ((sigma_tsq + sigma_xsq)*sigma_zsq)/(sigma_tsq + sigma_xsq + sigma_zsq)
+        sigma_t1sq = ((sigma_tsq + sigma_xsq)/(sigma_tsq + sigma_xsq + sigma_zsq))*sigma_zsq
+        sigma_tsq = sigma_t1sq
     list_sigma_tsq.append(sigma_tsq)
 #%%
 plt.plot(list_sigma_xsq, list_sigma_tsq)
