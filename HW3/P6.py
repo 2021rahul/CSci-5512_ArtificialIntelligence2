79299711026c6b9599b3788486784510df91019e
Rahul Ghosh <ghosh128@umn.edu>
2019-04-01 08:44:42 -0500

    result changes

--- HW3/P6.py
+++ HW3/P6.py
@@ -18,13 +18,20 @@ x = np.linspace(mu - 3*sigma, mu + 3*sigma, 100)
 plt.plot(x, stats.norm.pdf(x, mu, sigma))
 plt.show()
 #%%
-list_sigma_tsq = [1/2,1/2]
+list_sigma_tsq = [1/2]
 sigma_xsq = 1
 sigma_zsq = 0.5
 
-for t in range(2,100):
+for t in range(1,101):
     sigma_tsq = ((list_sigma_tsq[t-1] + sigma_xsq)/(list_sigma_tsq[t-1] + sigma_xsq + sigma_zsq))*sigma_zsq
     list_sigma_tsq.append(sigma_tsq)
 
+fig = plt.figure()
+fig.set_size_inches(20, 10)
+ax = plt.gca()
+ax.set_xticks(np.arange(1, 100, 1))
+ax.set_xticklabels(np.arange(1, 100, 1), rotation='vertical')
+plt.ylabel('variance')
+plt.xlabel('t')
 plt.plot(list_sigma_tsq[1:])
-plt.show()
+plt.title("variance vs time")
